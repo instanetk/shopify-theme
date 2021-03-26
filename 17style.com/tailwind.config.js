0c2f07c6a84290a5e4b90be04a7cdcd06c340c59
David Quintero <davidq@gmail.com>
2021-03-25 22:42:24 -0400

    add tailwind, create first section and template edit

---
+++ 17style.com/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [{ enabled: true, content: ['./**/*.liquid'] }],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
